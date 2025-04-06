c1709d36f335dd23c24bcfef51df633e02bd560c
Raj2601-Codingfortimepass <engrajeshdalai2601@gmail.com>
2025-04-06 12:22:44 +0530

    Update analyze_ss_movingload.py

--- analyze_ss_movingload.py
+++ analyze_ss_movingload.py
@@ -27,8 +27,8 @@ def analyze_beam(L, t1, t2, x):
     print(f"Maximum Reaction at B : {RB_max:.3f} kN")
     print(f"Shear Force at (0.5)L (SF_01) when t1 is at A: {SF_at_0:.3f} kN")  
     print(f"Bending Moment at A (BM_01) when t1 is at A: {BM_01:.3f} kNm")
-    print(f"Maximum Shear Force (SF_max): {SF_max:.2f} kN at {a_max:.3f} m from support A.")
-    print(f"Maximum Bending Moment at A (BM_max): {BM_max:.2f} kNm at {L - x:.3f} m from support A.")
+    print(f"Maximum Shear Force (SF_max): {SF_max:.2f} kN when the left weight is at {a_max:.3f} m from support A.")
+    print(f"Maximum Bending Moment at A (BM_max): {BM_max:.2f} kNm when the left weight is at {L - x:.3f} m from support A.")
     print("\n" + "_"*30)
 
 if __name__ == "__main__":
